cdb940772ad1865b9f3b6c3ddc27f313025bc6f4
hyunseung <ardimento22@naver.com>
2024-10-28 09:10:02 +0900

    init Project

---
+++ src/main/java/com/pratice/sortApp/SortAppApplication.java
@@ -0,0 +1,13 @@
+package com.pratice.sortApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SortAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pratice/sortApp/SortAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratice.sortApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
